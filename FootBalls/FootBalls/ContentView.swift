7bd7fbb3530117180a051ba54b13a9bbd70c941a
vcdog <vcdog@163.com>
2020-02-23 19:06:17 +0800

    FootBalls 概率预测神器

---
+++ FootBalls/FootBalls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FootBalls
+//
+//  Created by kevin lu on 2020/2/23.
+//  Copyright © 2020 kevin lu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
